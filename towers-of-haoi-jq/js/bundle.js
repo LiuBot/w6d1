55fbddf2ec28a0754fcf70ed89bf2c75418de2e0
Sophia Liu <sliu3@wellesley.edu>
2016-11-06 19:31:22 -0800

    view class of towers of hanoi game

--- towers-of-haoi-jq/js/bundle.js
+++ towers-of-haoi-jq/js/bundle.js
@@ -144,15 +144,30 @@
 	  	this.game = game;
 	  	this.$el = $el;
 	  	this.setupTowers();
+	  	this.render();
+	  	this.fromTowerIdx = null;
 	  }
+
+
+	// In the constructor, install a click handler on each pile. I wrote a 
+	// clickTower method. On the first click to a pile, get the pile number 
+	// and store this in an instance variable. On the second click (which 
+	// you can identify because the ivar has been set), perform the move. 
+	// Reset the ivar after. Alert the user if this was an invalid move.
+
+		clickTower(event){
+
+		}
+
+
+
+
 
 	// Write a View.prototype.setupTowers method to fill the main DOM 
 	// element with a "naive" representation of the game (i.e., not 
 	// reflecting the current game state). Use <ul> elements to store three 
 	// piles. Inside, use <li>s to store the discs. Call this in your 
 	// constructor.
-
-
 	  setupTowers(){
 	  	this.$el.empty();
 	  	this.$el.addClass("cf");
@@ -171,6 +186,40 @@
 	  	}
 		}
 
+
+
+
+	// Write a View.prototype.render to alter the DOM elements to reflect 
+	// the current game state. You should call this in your constructor, too.
+
+	// Used .removeClass: Remove a single class, 
+	// multiple classes, or all classes from each element in the set of matched elements.
+
+		render(){
+			const $towers = this.$el.find("ul");
+			$towers.removeClass();
+
+			if (this.fromTowerIdx !== null){
+				// eq method returns an element using a specific index number of the selected elements
+				$towers.eq(this.fromTowerIdx).addClass("selected");
+			}
+
+	    this.game.towers.forEach( (disks, towerIdx) => {
+	    	// .find() and .children() methods are similar, except that the latter only travels a single level down the DOM tree.
+	      const $disks = $towers.eq(towerIdx).children(); // children = the disks at that stack
+	      $disks.removeClass();
+
+	      disks.forEach( (diskWidth, diskIdx) => {
+	        /*
+	        Since our disks are stacked from bottom to top
+	        as [3, 2, 1], we have to select from the back
+	        of our jQuery object, using negative indices.
+	        */
+	        $disks.eq(-1 * (diskIdx + 1)).addClass(`disk-${diskWidth}`);
+		
+		      });
+	    });
+	  }
 
 
 	}
